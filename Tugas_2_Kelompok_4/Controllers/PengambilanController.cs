3e687ca501ee55a4114abd4088989d0115fd5b0a
NuraKhofifah <nurawartami@gmail.com>
2023-10-06 15:47:05 +0700

    menambahkan model dan controller transaksi pengambilan

---
+++ Tugas_2_Kelompok_4/Controllers/PengambilanController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tugas_2_Kelompok_4.Models;
+
+namespace Tugas_2_Kelompok_4.Controllers
+{
+    public class PengambilanController : Controller
+    {
+        private static List<Pengambilan> pengambilans = InitializeData();
+        private static int currentId = 1;
+
+        private static List<Pengambilan> InitializeData()
+        {
+            List<Pengambilan> initialData = new List<Pengambilan>
+            {
+                new Pengambilan
+                {
+                    id_pengambilan = "PGN001",
+                    jenis = "Barang",
+                    nama_pemilik = "Mukhasim",
+                    nama_pengambil = "Mukhasim",
+                    nim_npk = "0320220134",
+                    tanggal_pengambilan = new DateTime(2023, 9, 28)
+                },
+                new Pengambilan
+                {
+                    id_pengambilan = "PGN002",
+                    jenis = "Dokumen",
+                    nama_pemilik = "Rafa",
+                    nama_pengambil = "Dinda",
+                    nim_npk = "0420220134",
+                    tanggal_pengambilan = new DateTime(2023, 9, 29)
+                }
+            };
+            currentId = 3; // Mengatur id berikutnya untuk data yang akan ditambahkan.
+            return initialData;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Pengambilan> pengambilanList = pengambilans.ToList();
+            return View(pengambilanList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pengambilan pengambilan)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine(pengambilan.tanggal_pengambilan);
+                string newId = $"PGN{currentId:D3}"; // Membuat id dengan format PKTXXX
+
+                pengambilan.id_pengambilan = newId;
+
+                pengambilans.Add(pengambilan);
+                TempData["SuccessMessage"] = "Data berhasil ditambahkan";
+                currentId++; // Menambahkan id berikutnya
+                return RedirectToAction("Index");
+            }
+
+            return View(pengambilan);
+        }
+    }
+}
--- Tugas_2_Kelompok_4/Models/Pengambilan.cs
+++ Tugas_2_Kelompok_4/Models/Pengambilan.cs
@@ -4,12 +4,36 @@ namespace Tugas_2_Kelompok_4.Models
 {
     public class Pengambilan
     {
-        public string? id_jenis { get; set; }
+        public string? id_pengambilan { get; set; }
 
         [Required(ErrorMessage = "Jenis paket wajib diisi.")]
         [MaxLength(30, ErrorMessage = "Jenis paket maksimal 30 karakter")]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Jenis paket hanya boleh berisi huruf.")]
-        public string nama_jenis { get; set; }
+        public string jenis { get; set; }
+
+        [Required(ErrorMessage = "Nama pemilik wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama pemilik maksimal 30 karakter.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nama pemilik hanya boleh berisi huruf.")]
+        public string nama_pemilik { get; set; }
+
+        [Required(ErrorMessage = "Nama pengambil wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Nama pengambil maksimal 30 karakter.")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nama pengambil hanya boleh berisi huruf.")]
+        public string nama_pengambil { get; set; }
+
+        [Required(ErrorMessage = "NIM/NPK wajib diisi.")]
+        [MaxLength(11, ErrorMessage = "NIM/NPK maksimal 11 karakter")]
+        public string nim_npk { get; set; }
+
+        [Required(ErrorMessage = "Tanggal pengambilan sampai wajib diisi.")]
+        [DataType(DataType.Date)]
+        public DateTime tanggal_pengambilan { get; set; }
+
+        
+
+        
+
+        
 
     }
 }
