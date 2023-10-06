d950bfc3cefa10cd83393e30886f70f82b44752a
madedanan <jayadanan82@gmail.com>
2023-10-06 08:31:57 +0700

    tampilan transaksi

---
+++ Tugas_2_Kelompok_4/Models/Pengambilan.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tugas_2_Kelompok_4.Models
+{
+    public class Pengambilan
+    {
+        public int id_jenis { get; set; }
+
+        [Required(ErrorMessage = "Jenis paket wajib diisi.")]
+        [MaxLength(30, ErrorMessage = "Jenis paket maksimal 30 karakter")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Jenis paket hanya boleh berisi huruf.")]
+        public string nama_jenis { get; set; }
+    }
+}
